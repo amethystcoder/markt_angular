d16c39c225f9d2c06c11bff1443aad9465b3d4ed
Joshua <amethystguitaristj@gmail.com>
2023-07-24 11:48:10 +0100

    changes to the product, display and order service

--- src/app/userstate.service.ts
+++ src/app/userstate.service.ts
@@ -12,11 +12,17 @@ export class UserstateService {
   user_name = new BehaviorSubject<string>("")
   user_id = new BehaviorSubject<string>("")
   user_profile_image = new BehaviorSubject<string>("")
+  longtitude = new BehaviorSubject<number>(0)
+  latitude = new BehaviorSubject<number>(0)
+  //chatstate = new BehaviorSubject<string>("closed")
 
   user_type_sub = this.user_type.asObservable()
   user_name_sub = this.user_name.asObservable()
   user_id_sub = this.user_id.asObservable()
   user_profile_image_sub = this.user_profile_image.asObservable()
+  longtitude_sub = this.longtitude.asObservable()
+  latitude_sub = this.latitude.asObservable()
+  //chatstate_sub = this.chatstate.asObservable()
 
   setuser(user_type:string,user_name:string,user_id:string,user_profile_image:string){
     this.user_id.next(user_id)
