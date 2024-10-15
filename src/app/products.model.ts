1af6d95c368a45d0e3eb3e67d3569952e0a38a3f
SeniorCub <farinderif@gmail.com>
2024-10-15 23:04:02 +0100

    Refactor user model and fix formatting issues

--- src/app/products.model.ts
+++ src/app/products.model.ts
@@ -11,45 +11,45 @@ export interface Category{
   export interface Catg{
     categories: Array<any>
   }
-  
+
   export interface Product{
-    product_id:string, 
+    product_id:string,
     product_name:string,
     product_type:string,
-    product_price:number, 
+    product_price:number,
     product_description:string,
-    product_category:string, 
+    product_category:string,
     tags:Array<string>,
     product_images: Array<string>,
-    product_quantity:number, 
+    product_quantity:number,
     estimated_size:number,
     seller_id:string,
     seller_name:string,
     desc_under:string,
     favorite: boolean
   }
-  
+
   export interface CartItem{
     cart_id:string,
-    quantity:number,
     product_image:string,
     product_name:string,
     product_type:string,
     product_price:number,
     product_id:string,
+    quantity:number,
     has_discount:boolean,
     discount_price:number,
     discount_percent:number
   }
-  
+
   export interface ProductQuery{
     query_id:string,
-    buyer_id:string, 
+    buyer_id:string,
     buyer_name:string,
     city:string,
     state:string,
     date_created:string,
-    message:string, 
+    message:string,
     category:string,
     stale_time:number,
     profile_image:string
