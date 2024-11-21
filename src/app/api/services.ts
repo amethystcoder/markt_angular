b5962369846b7fe47f2b0451073502e646035b14
DevHeris <badairoinioluwa578@gmail.com>
2024-11-21 09:17:31 +0100

    SORT

--- src/app/api/services.ts
+++ src/app/api/services.ts
@@ -1,7 +1,7 @@
-export { PasswordResetService } from './services/password-reset.service';
-export { FavoritesService } from './services/favorites.service';
-export { ProductService } from './services/product.service';
-export { CommentService } from './services/comment.service';
-export { OrderService } from './services/order.service';
 export { CartService } from './services/cart.service';
+export { CommentService } from './services/comment.service';
+export { FavoritesService } from './services/favorites.service';
+export { OrderService } from './services/order.service';
+export { PasswordResetService } from './services/password-reset.service';
+export { ProductService } from './services/product.service';
 export { UserService } from './services/user.service';
