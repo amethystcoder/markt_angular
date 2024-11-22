93e36d4e48445e2782661ce547f711ca06fa265a
Joshua <amethystguitaristj@gmail.com>
2024-11-22 20:48:28 +0000

    examle structure of endpoint

--- src/app/services/apiSpecificData.ts
+++ src/app/services/apiSpecificData.ts
@@ -1,8 +1,8 @@
 
 export const ApiStore = {
     api: "http://localhost:5000/",
-    possibleEndpoints:["auth","products","cart"],
-    mergeEndpoint: (endpoint:string)=>{
-        return ApiStore.api + ApiStore.possibleEndpoints.filter((value)=>value == endpoint)
+    possibleEndpoints:["auth","products","cart","comments","favorites","psw_ret","orders","product_request","user"],
+    mergeEndpoint: (endpoint:string,...points:string[])=>{
+        return ApiStore.api + ApiStore.possibleEndpoints.find((value)=>value == endpoint) + "/" + points.join("/")
     }
 }
--- src/app/services/http/auth.service.ts
+++ src/app/services/http/auth.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, inject } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
-import { Observable, retry, tap } from 'rxjs';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { EMPTY, Observable, catchError, retry, tap } from 'rxjs';
 import { ApiStore } from "../apiSpecificData";
+import { UserLogin, UserLoginResponse } from '../../api/models';
 
 @Injectable({
   providedIn: 'root'
@@ -12,12 +13,29 @@ export class AuthServiceService {
 
   http = inject(HttpClient)
 
-  authenticateUser(authData:any):Observable<any>{
-    return this.http.post(ApiStore.mergeEndpoint("api"),authData,{
+  authenticateUser(authData:UserLogin):Observable<HttpResponse<UserLoginResponse>>{
+    return this.http.post<UserLoginResponse>(ApiStore.mergeEndpoint("auth","login"),authData,{
       observe:"response"
     }).pipe(
       tap((data)=>console.log(data)),
-      retry(3)
+      retry(3),
+      catchError(err => {
+        console.error(err);
+        return EMPTY;
+      })
+    )
+  }
+
+  logoutUser():Observable<HttpResponse<UserLoginResponse>>{
+    return this.http.post<UserLoginResponse>(ApiStore.mergeEndpoint("auth","logout"),{},{
+      observe:"response"
+    }).pipe(
+      tap((data)=>console.log(data)),
+      retry(3),
+      catchError(err => {
+        console.error(err);
+        return EMPTY;
+      })
     )
   }
 }
