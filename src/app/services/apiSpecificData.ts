ec28b2e2882cc80ce5c79499e675b6bd6d6dad2a
Joshua <amethystguitaristj@gmail.com>
2024-11-22 09:11:36 +0000

    small change

--- src/app/services/apiSpecificData.ts
+++ src/app/services/apiSpecificData.ts
@@ -1,7 +1,7 @@
 
 export const ApiStore = {
     api: "http://localhost:5000/",
-    possibleEndpoints:[],
+    possibleEndpoints:["auth","products","cart"],
     mergeEndpoint: (endpoint:string)=>{
         return ApiStore.api + ApiStore.possibleEndpoints.filter((value)=>value == endpoint)
     }
