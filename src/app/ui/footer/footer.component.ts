6b99e2c607e69ea74bc38d3ddab0c23855dc51ff
DevHeris <badairoinioluwa578@gmail.com>
2024-12-02 20:18:18 +0100

    SIGNUP PAGE UI COMPLETION

--- src/app/ui/footer/footer.component.ts
+++ src/app/ui/footer/footer.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
   imports: [],
   templateUrl: './footer.component.html',
-  styleUrl: './footer.component.css'
+  styleUrl: './footer.component.css',
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit {
+  currentYear!: number;
 
+  ngOnInit(): void {
+    this.currentYear = new Date().getFullYear();
+  }
 }
